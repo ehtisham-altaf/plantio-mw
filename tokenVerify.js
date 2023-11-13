74aa4e36d393e0df0450a264a546e3712c42edfc
ehtisham-altaf <ehtisham.altaf@aiondigital.com>
2023-11-13 21:37:41 +0500

    added netlify data for production

--- tokenVerify.js
+++ tokenVerify.js
@@ -4,7 +4,7 @@ const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if(authHeader)
     {
-        token = authHeader.split(" ")[1];
+        token = authHeader;
         jwt.verify(token,process.env.JWT_SECRET_KEY,(err,user)=>{
             if(err)
             {
